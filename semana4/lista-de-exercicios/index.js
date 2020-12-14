b33657013e5168d8b76bb137a9f5b5cac531f009
Allison Araujo <allisonaraujo94@outlook.com>
2020-12-13 22:47:27 -0300

    Exercícios de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,33 @@
+// # Lista de Exercícios:
+
+// Exercícios de interpretação de código
+
+    // 1.
+    // o exercício tem uma função para converter a moeda do dólar
+    // solicita que o usúario digite o valor da cotação do Dólar atual
+    // e a função retorna o calculo já montado
+    // E depois armazenou este return dentro de uma variavel chamada meuDinheiro, e inseriu um valor
+    // de 100, para cotação. E deu um console.log para imprimir o resultado, com o valor inserido.
+
+
+    // 2.
+    // o exercício tem uma função que tem 2 parametros um, com o tipo de investimento e o valor que será multiplicado
+    // e seguida declara uma variavel para ser o resultado, utiliza switch para identificar qual tipo de investimento será usado
+    // fazendo as devidas multiplicações na qual é retornado na variavel ValorAposIvestimento e é retornado.
+    // Depois chama-se as funções com os valores pré fixados, e imprime na tela.
+    // Onde o primeiro valor é impresso 156, e no segundo valor é impresso "TIPO DE INVESTIMENTO INFORMADO INCORRETO"
+
+    // 3.
+    // O exercício inicia com três variaveis apontado para arrays, onde obtém valores dentro e os demais estão vazios.
+    // Em seguida pecorre o array da variavel "numeros", e verifica quais numeros que o resto da divisão é igual a 0, e armazena esses resultados
+    // na array1, que estava vazia, e os numeros que no resto da divisão é igual a 1, é armazenado na array2.
+
+    // 4.
+    // O exercício inicialmente declara uma variavel com um array,
+    // declara uma segunda variavel numero1, com uma palavra reservada, na qual
+    // o seu valor é maior que qualquqer numero.
+    // e a terceira variavel igual a zero.
+    // Em seguida, pecorre a array numero e faz duas verificações.
+    // a Primeira: qual é o numero menor do array.
+    // e a segunda é: qual maior numero da array.
+    // e depos é impresso no console, os dois resultados.
