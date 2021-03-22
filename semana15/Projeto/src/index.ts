bf497ba82e7568094cd97896aeed5ef5d9e59b63
Allison Araujo <allisonaraujo94@outlook.com>
2021-03-21 21:27:14 -0300

    projeto api bancario

--- semana15/Projeto/src/index.ts
+++ semana15/Projeto/src/index.ts
@@ -1,16 +1,54 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
+import { accounts } from './accounts'
 
 const app: Express = express()
 
 app.use(express())
 app.use(cors())
 
-app.get("/", ( req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.sendStatus(200)
 })
 
+app.post("/users/create", (req: Request, res: Response) => {
+    try {
+        const {nome, CPF, dateOfBirthAsString } = req.body
 
-app.listen(3003, ()=>{
+        const [day, month, yeah] = dateOfBirthAsString.split('/')
+
+        const dateOfBirth: Date = new Date(`${yeah}-${month}-${day}`)
+
+        accounts.push({
+            name: nome,
+            CPF,
+            dateOfBirth,
+            balance: 0,
+            statement: [],
+        })
+
+        res.status(400).send("Conta criada.")
+
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+})
+
+app.get("/users/all", (req: Request, res: Response) => {
+    try {
+
+        if (!accounts.length) { 
+            res.statusCode = 404
+            throw new Error("Nenhuma conta foi encontrada.")
+        }
+
+        res.status(200).send(accounts)
+
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.listen(3003, () => {
     console.log("Servidor na port: 3003 => OK :)")
 })
